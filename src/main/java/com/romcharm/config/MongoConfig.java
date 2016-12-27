ae8158d88cbbb46426da1a72f1a91b4a3068cf53
Romesh <rselvanathan@omnifone.com>
2016-12-27 02:09:52 +0000

    Added Authentication to MongoDB

--- src/main/java/com/romcharm/config/MongoConfig.java
+++ src/main/java/com/romcharm/config/MongoConfig.java
@@ -2,9 +2,13 @@ package com.romcharm.config;
 
 import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+
+import java.util.Collections;
 
 @Configuration
 public class MongoConfig extends AbstractMongoConfiguration {
@@ -15,8 +19,14 @@ public class MongoConfig extends AbstractMongoConfiguration {
     @Value("${MONGODB_PORT_27017_TCP_PORT}")
     private int port;
 
+    @Value("${MONGODB_USERNAME}")
+    private String username;
+
     @Value("${MONGODB_DBNAME}")
     private String mongoDatabaseName;
+
+    @Value("${MONGODB_PASSWORD}")
+    private String password;
 
     @Override
     protected String getDatabaseName() {
@@ -25,6 +35,7 @@ public class MongoConfig extends AbstractMongoConfiguration {
 
     @Override
     public Mongo mongo() throws Exception {
-        return new MongoClient(host, port);
+        return new MongoClient(new ServerAddress(host, port),
+                Collections.singletonList(MongoCredential.createCredential(username, "admin", password.toCharArray())));
     }
 }

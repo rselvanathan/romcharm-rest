e0cf15ca7ea6461da4e02ea99ce091fa68fe7297
Romesh <rselvanathan@omnifone.com>
2017-01-02 01:15:40 +0000

    Added custom way of determining whether a list of CORS origins are allowed, due to filter limitations.

--- src/main/java/com/romcharm/config/CorsFilterConfig.java
+++ src/main/java/com/romcharm/config/CorsFilterConfig.java
@@ -8,10 +8,23 @@ import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @Component
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorsFilterConfig implements Filter {
+
+    private final List<String> accessAllowOrigins;
+
+    public CorsFilterConfig() {
+        this.accessAllowOrigins = Stream.of("http://www.romandcharmi.com",
+                                            "http://romandcharmi.com")
+                                        .collect(Collectors.toList());
+    }
+
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
     }
@@ -20,7 +33,14 @@ public class CorsFilterConfig implements Filter {
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletResponse response = (HttpServletResponse) servletResponse;
         HttpServletRequest request = (HttpServletRequest) servletRequest;
-        response.setHeader("Access-Control-Allow-Origins", "http://www.romandcharmi.com, http://romandcharmi.com");
+
+        String origin = "";
+        String requestOrigin = request.getHeader("Origin");
+        if(accessAllowOrigins.contains(requestOrigin)){
+            origin = requestOrigin;
+        }
+
+        response.setHeader("Access-Control-Allow-Origin", origin);
         response.setHeader("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS");
         response.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
 
